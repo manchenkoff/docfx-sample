751526ca9020b91a4039ac2c04fa696505d28b00
Artem Manchenkov <amanchenkov@microsoft.com>
2023-02-03 15:54:55 +0100

    configured docfx for classes

--- User.cs
+++ User.cs
@@ -1,11 +1,25 @@
 ﻿namespace DocGenSample
 {
+    /// <summary>
+    /// Example of documentation for 'User' public class
+    /// </summary>
     public class User
     {
+        /// <summary>
+        /// Unique user identifier
+        /// </summary>
         public int Id { get; set; }
 
+        /// <summary>
+        /// Full name of the user
+        /// </summary>
         public string Name { get; set; }
 
+        /// <summary>
+        /// Creates a new instance of User object
+        /// </summary>
+        /// <param name="id">Unique identifier</param>
+        /// <param name="name">Full name</param>
         public User(int id, string name)
         {
             Id = id;
