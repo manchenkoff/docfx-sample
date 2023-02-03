b478cf433d909058091772d1c9bdd0e1c0fe3b13
Artem Manchenkov <amanchenkov@microsoft.com>
2023-02-03 16:30:14 +0100

    added mermaid support and ignored internal class

---
+++ RandomInternalClass.cs
@@ -0,0 +1,16 @@
+﻿namespace DocGenSample
+{
+    /// <summary>
+    /// Doc line for internal class
+    /// </summary>
+    internal class RandomInternalClass
+    {
+        /// <summary>
+        /// Comment for Foo method
+        /// </summary>
+        public void Foo()
+        {
+            return;
+        }
+    }
+}
--- articles/intro.md
+++ articles/intro.md
@@ -1,1 +1,11 @@
 # Add your introductions here!
+
+```mermaid
+sequenceDiagram
+
+participant a as Client
+participant b as Server
+
+a->>b: request
+b->>a: response
+```
